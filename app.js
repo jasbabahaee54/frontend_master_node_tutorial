03cf6b0f16085f06b6a106cb83b4fcdbd13c2059
Ronit Panda <pandaronit25@gmail.com>
2021-06-27 11:51:39 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const data2 = "Yo boy whats up";
+const data3 = "You are good";
+
+console.log("Start");
+fs.writeFile('./text2.txt', data2, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("mid");
+    }
+});
+console.log("end");
